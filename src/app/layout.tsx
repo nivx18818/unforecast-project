13215c8ab695ec34864217c657886d8c7ab82329
NguyenXuanVinh <nongami87@gmail.com>
2026-03-07 21:18:20 +0700

    feat: integrate shadcn/ui with theming and utility setup
    - integrates shadcn/ui and Hugeicons.
    - updates .gitignore for better IDE and env file support.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Playfair_Display } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const playfairDisplay = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-serif",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-serif", playfairDisplay.variable)}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
